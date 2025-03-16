2c45c5f2165a1ad702c27430619e3b807656fd24
Emiel Doek <57521297+EmielDoek@users.noreply.github.com>
2025-03-16 01:46:33 +0000

    Prototype of Gaseous Plant implemented but still needs tweaking
    this prototypes functionality is made using C++, for example, it requires the player character to not be on the ground for the functionality to actually work.

---
+++ Source/CollabGroup06Project/GaseousPlant.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GaseousPlant.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+AGaseousPlant::AGaseousPlant()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+}
+
+// Called when the game starts or when spawned
+void AGaseousPlant::BeginPlay()
+{
+	Super::BeginPlay();
+	Collider = GetComponentByClass<UBoxComponent>();
+	Collider->OnComponentBeginOverlap.AddDynamic(this, &AGaseousPlant::AGaseousPlant::OnOverlapBeginBox);
+	Collider->OnComponentEndOverlap.AddDynamic(this, &AGaseousPlant::AGaseousPlant::OnOverlapEndBox);
+}
+
+// Called every frame
+void AGaseousPlant::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AGaseousPlant::OnOverlapBeginBox(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ACharacter* OtherCharacter = Cast<ACharacter>(OtherActor);
+	if (OtherCharacter != nullptr)
+	{
+		OtherCharacter->GetCharacterMovement()->AddForce(FVector(0,0,-10000));
+		OtherCharacter->GetCharacterMovement()->GravityScale = -1.00;
+		OtherCharacter->GetVelocity().Set(0,0,100.0);
+		
+	}
+}
+
+void AGaseousPlant::OnOverlapEndBox(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	ACharacter* OtherCharacter = Cast<ACharacter>(OtherActor);
+	if (OtherCharacter != nullptr)
+	{
+		OtherCharacter->GetCharacterMovement()->GravityScale = 1.75;
+	}
+	
+}
+
--- Source/CollabGroup06Project/GaseousPlant.h
+++ Source/CollabGroup06Project/GaseousPlant.h
@@ -6,31 +6,27 @@
 #include "GameFramework/Actor.h"
 #include "GameFramework/Character.h"
 #include "Components/BoxComponent.h"
-#include "LaunchPad.generated.h"
-
-
+#include "GaseousPlant.generated.h"
 
 UCLASS()
-class COLLABGROUP06PROJECT_API ALaunchPad : public AActor
+class COLLABGROUP06PROJECT_API AGaseousPlant : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	ALaunchPad();
-
+	AGaseousPlant();
+	UPROPERTY(EditAnywhere, Category="Collider Object") UBoxComponent* Collider;
+	UPROPERTY(EditAnywhere, Category="Collider Object") float gravity;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	UPROPERTY(EditAnywhere, Category="Collider Object") UBoxComponent* Collider;
-	UPROPERTY(EditAnywhere, Category="Launch Velocity") int32 M_LaunchVelocity = 750;
-	//Positional overrides for the launching of the character
-	UPROPERTY(EditAnywhere, Category="Position Override") bool XYOverride = false;
-	UPROPERTY(EditAnywhere, Category="Position Override") bool ZOverride = true;
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 	UFUNCTION()
 	void OnOverlapBeginBox(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	
+	UFUNCTION()
+	void OnOverlapEndBox(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };
