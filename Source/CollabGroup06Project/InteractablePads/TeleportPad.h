e22e0f08d8e2e4b5db30306c666226155f0ccf0b
Luke.Taylor <94555095+Luke-Taylor-Coding@users.noreply.github.com>
2025-03-26 19:06:31 +0000

    Added Teleport Class
    Class inherits from interactable pad class

---
+++ Source/CollabGroup06Project/InteractablePads/TeleportPad.h
@@ -0,0 +1,15 @@
+﻿
+#pragma once
+
+#include "InteractPad_Base.h"
+#include "TeleportPad.generated.h"
+
+
+UCLASS()
+class COLLABGROUP06PROJECT_API ATeleportPad : public AInteractPad_Base
+{
+	GENERATED_BODY()
+
+public:
+	
+};
