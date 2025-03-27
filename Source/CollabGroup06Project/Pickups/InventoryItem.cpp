5f4a6c3aead5c5b39a19bfcf6f047c00e94d8fcb
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2025-03-27 01:05:00 +0000

    Work on Creating Inventory System

---
+++ Source/CollabGroup06Project/Pickups/InventoryItem.cpp
@@ -0,0 +1,17 @@
+﻿#include "InventoryItem.h"
+
+
+AInventoryItem::AInventoryItem()
+{
+	_Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = _Root;
+	
+	_ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pickup Item Mesh"));
+	_ItemMesh->SetupAttachment(RootComponent);
+}
+
+void AInventoryItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/CollabGroup06Project/Pickups/InventoryItem.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "../Player/PlayerTools/ItemData.h"
+#include "InventoryItem.generated.h"
+
+UCLASS()
+class COLLABGROUP06PROJECT_API AInventoryItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AInventoryItem();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UStaticMeshComponent> _ItemMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USceneComponent> _Root;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Inventory Item Properties", meta=(ExposeOnSpawn=true))
+	FItemData _ItemData;
+
+protected:
+	virtual void BeginPlay() override;
+};
